0d59771f34ce387a982c3996236aaeabb6089347
tanjd <42729752+tanjd@users.noreply.github.com>
2021-10-17 22:51:52 +0800

    connect to database using compose-file and entrypoint.sh to start flask application

---
+++ ecommerce_test/user/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export FLASK_ENV=development
+export FLASK_APP=user.py
+flask db init
+flask db migrate
+flask db upgrade
+flask run --host=0.0.0.0
--- ecommerce_test/user/user.py
+++ ecommerce_test/user/user.py
@@ -1,9 +1,37 @@
+from os import environ
+
 from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_migrate import Migrate
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("dbURL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 CORS(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+
+    def json(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password,
+            "name": self.name,
+            "role": self.role,
+            "created_at": self.created_at,
+        }
 
 
 @app.route("/")
@@ -18,11 +46,13 @@ def test():
 
 @app.route("/users")
 def users():
-    users = [
-        {"name": "John", "school": "SCIS"},
-        {"name": "Jane", "school": "SCIS"},
-        {"name": "Richard ", "school": "SCIS"},
-    ]
+    # users = [
+    #     {"name": "John", "school": "SCIS"},
+    #     {"name": "Jane", "school": "SCIS"},
+    #     {"name": "Richard ", "school": "SCIS"},
+    # ]
+
+    users = {"user": [User.json() for User in User.query.all()]}
     response = make_response(
         jsonify({"status": "success", "data": users}),
         200,
