eb1d70b84b690da8c7eeb138fad9a45ee659618b
tanjd <42729752+tanjd@users.noreply.github.com>
2021-10-24 11:26:42 +0800

    clean autoload.js

--- app/js/autoload.js
+++ app/js/autoload.js
@@ -30,16 +30,6 @@ function getCookie(cname) {
     }
   }
   return "";
-
-  // for (var i = 0; i < ca.length; i++) {
-  //   var c = ca[i];
-  //   while (c.charAt(0) == " ") {
-  //     c = c.substring(1);
-  //   }
-  //   if (c.indexOf(name) == 0) {
-  //     return c.substring(name.length, c.length);
-  //   }
-  // }
 }
 
 function deleteCookie(name) {
